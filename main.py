d8a3a37dfc0692975aae467f1eab1f3b490d989c
Shayan Azizi <shayanazizi1386@gmail.com>
2022-11-30 18:17:21 +0330

    Add all feature in top goal scorer function

--- main.py
+++ main.py
@@ -205,16 +205,23 @@ def profile_competition (team1 : str, team2 : str) -> None:
 # The best Functions
 
 def top_goal_scorer (year : str) -> None:
-    if year == "all":
-        
-    
-    results = search_in (goals_database, year, 4)
-    
     scorers = []
     
-    for result in results:
-        scorers.append (result[8])
+    if year == "all":
+        
+        for line in goals_database:
+            line = line.split(",")
+            
+            scorers.append (line[8])
+            
+            
+    else:
+        results = search_in (goals_database, year, 4)
     
+        
+        for result in results:
+            scorers.append (result[8])
+        
     top_goal_scorer = most_frequent(scorers)
     
     print (f"{search_ex(players_database, top_goal_scorer, 0)[0][2]} {search_ex(players_database, top_goal_scorer, 0)[0][1]}")
